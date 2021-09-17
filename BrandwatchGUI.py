48960471ad23962b5f32c876b454408d581ca4e3
HurinHu <hurin@live.ca>
2021-09-17 12:54:15 +1200

    disable folder open if path is not exit

--- BrandwatchGUI.py
+++ BrandwatchGUI.py
@@ -429,10 +429,11 @@ class Downloader(QFrame):
                 filepath = os.path.dirname(self.savePath)
             else:
                 filepath = self.savePath
-            if platform.system() == 'Windows':
-                os.system(f'start {os.path.realpath(filepath)}')
-            else:
-                os.system('xdg-open "%s"' % filepath)
+            if os.path.exists(filepath):
+                if platform.system() == 'Windows':
+                    os.system(f'start {os.path.realpath(filepath)}')
+                else:
+                    os.system('xdg-open "%s"' % filepath)
 
     # If the format is changed, the ext can be changed automatically
     def changeFormat(self):
